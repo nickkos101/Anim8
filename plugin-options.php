eb67d44d32da677be51b201a5a221eecd30bfd8a
Nicholas Koskowski <Nick@businessonmarketst.com>
2013-08-22 11:20:18 -0700

    1.1 Update Complete
    this is RC candidate.

--- plugin-options.php
+++ plugin-options.php
@@ -2,25 +2,26 @@
 function anim8_options_do_page() {    ?>
 <div class="wrap">
 	<?php if (false !== $_REQUEST['settings-updated']) : ?>
-	<div class="updated fade"><p><strong><?php _e('Options saved', 'anim8theme'); ?></strong></p></div>
-<?php endif; ?> 
-<form method="post" action="options.php">
-	<?php settings_fields('anim8_options'); ?>
-	<?php $options = get_option('anim8_theme_options'); ?>
-	<h3>Homepage Content Area 1</h3>
-	<h4>Content Title</h4>
-	<input type="text" size="36" name="anim8_theme_options[p1title]" value="<?php esc_attr_e($options['p1title']); ?>" />                 
-	<h4>Content Text:</h4>
-	<textarea id="anim8_theme_options[p1text]" class="large-text" style="height:130px; width:375px;" name="anim8_theme_options[p1text]"><?php echo esc_textarea($options['p1text']); ?></textarea>
-	<p class="submit">
-		<input type="submit" class="button-primary" value="<?php _e('Save Options', 'anim8theme'); ?>" />
-	</p>
-
-</form>
+		<div class="updated fade"><p><strong><?php _e('Options saved', 'anim8theme'); ?></strong></p></div>
+	<?php endif; ?> 
+	<form method="post" action="options.php">
+		<?php settings_fields('anim8_options'); ?>
+		<?php $options = get_option('anim8_theme_options'); ?>
+		<a href="http://businessonmarketst.com"><img src="<?php echo plugins_url('/anim8/Images/logo.png'); ?>"></a>
+		<p>We believe that every activity ties together and revolves around the five basic disciplines of business: planning and research, branding, web development, marketing, and business development. Once these elements are working in concert, we find that organic solutions rise easily to the surface. </p>
+		<h4>Slider Width</h4>
+		<input type="text" size="2" name="anim8_theme_options[width]" value="<?php esc_attr_e($options['width']); ?>" />
+		<h4>Slider Height</h4>
+		<input type="text" size="2" name="anim8_theme_options[height]" value="<?php esc_attr_e($options['height']); ?>" />
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Options', 'anim8theme'); ?>" />
+		</p>
+		<img src="<?php echo plugins_url('/anim8/Images/nick.jpeg'); ?>">
+		<p>Developed By: Nicholas Koskowski, <a href="https://github.com/nickkos101/">check me out on github</a></p>
+	</form>
 </div>
 <?php
 }
-
 function anim8_options_validate($input) {
 	global $select_options, $radio_options;
     // Our checkbox value is either 0 or 1
